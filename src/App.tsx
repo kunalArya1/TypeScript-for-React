fbfc4c8197c8dc5824a1b4be2f110dc228d2ea38
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2024-03-12 14:01:05 +0530

    Component Props done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
 import "./App.css";
 
-function App() {
-  return (
-    <>
-      <h1>Hello TypeScript in react </h1>
-      <MyComponent prop1="Kunal Arya" />
-    </>
-  );
-}
-
 type MyComponentProps = {
   prop1: string;
 };
@@ -16,5 +7,32 @@ type MyComponentProps = {
 const MyComponent: React.FC<MyComponentProps> = ({ prop1 }) => {
   return <h1> {prop1}</h1>;
 };
+
+type QuizComponentProps = {
+  prop1: {
+    question: string;
+    answer: string;
+  };
+};
+
+const QuizComponent: React.FC<QuizComponentProps> = ({ prop1 }) => {
+  return (
+    <div>
+      <h1> {prop1.question}</h1>
+      <h1> {prop1.answer}</h1>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <>
+      <h1>Hello TypeScript in react </h1>
+      <MyComponent prop1="Kunal Arya" />
+
+      <QuizComponent prop1={{ question: "what is ram", answer: "god" }} />
+    </>
+  );
+}
 
 export default App;
